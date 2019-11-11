825e07007f66e76a835289360dd5ee83f5cef298
palhamel <pal.hamel@gmail.com>
2019-11-11 18:08:08 +0100

    Adds happy path test

--- spec/ATM_spec.rb
+++ spec/ATM_spec.rb
@@ -8,6 +8,21 @@ describe ATM do
     it 'funds are reduced at withdraw' do
         subject.withdraw 50
         expect(subject.funds).to eq 950
-      end
+    end
+
+    it 'prints a receipt'
 end
 
+describe ATM do
+    let(:account) { instance_double('Account') }
+  
+    before do
+      # Before each test we need to add an attribute of `balance`
+      # to the `account` object and set the value to `100`
+      allow(account).to receive(:balance).and_return(100)
+      # We also need to allow `account` to receive the new balance
+      # using the setter method `balance=`
+      allow(account).to receive(:balance=)
+    end
+    
+  end
