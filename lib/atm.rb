aa8776ff6a89aaf7b306305a0df2d2c6ec241e06
your_name <your@email.com>
2019-11-12 10:35:54 +0100

    adds account to withdrawl method

--- lib/atm.rb
+++ lib/atm.rb
@@ -5,8 +5,24 @@ class ATM
         @funds = 1000
     end
 
-    def withdraw(amount)
-        @funds -= amount    
-    end
+    def withdraw(amount, account)
+        # We will be using Ruby's `case`- `when` - `then` flow control statement
+        # and check if there are enough funds in the account
+        case
+        when amount > account.balance
+          # we exit the method if the amount we want to withdraw is 
+          # bigger than the balance on the account
+          return
+        else
+          # If it's not, we perform the transaction
+          # We DEDUCT the amount from the Atm's funds
+          @funds -= amount
+          # We also DEDUCT the amount from the accounts balance
+          account.balance = account.balance - amount
+          # and we return a response for a successful withdraw.
+          { status: true, message: 'success', date: Date.today, amount: amount }
+        end
+      end
 
 end
+
--- spec/ATM_spec.rb
+++ spec/ATM_spec.rb
@@ -23,6 +23,22 @@ describe ATM do
       # We also need to allow `account` to receive the new balance
       # using the setter method `balance=`
       allow(account).to receive(:balance=)
+      
     end
     
+    it 'allow withdraw if the account has enough balance.' do
+      # We need to tell the spec what to look for as the response
+      # and store that in a variable called `expected_outcome`.
+      # Please note that we are omitting the `bills` part at the moment,
+      # We will modify this test and add that later.
+  
+      expected_output = { status: true, message: 'success', date: Date.today, amount: 45 }
+  
+      # We need to pass in two arguments to the `withdraw` method.
+      # The amount of money we want to withdraw AND the `account` object.
+      # The reason we pass in the `account` object is that the Atm needs
+      # to be able to access information about the `accounts` balance
+      # to be able to clear the transaction.
+      expect(subject.withdraw(45, account)).to eq expected_output
+    end
   end
