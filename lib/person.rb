af5e47df78f5741e73902245e7a4e8c7fd50bc1a
palhamel <pal.hamel@gmail.com>
2019-11-13 16:04:46 +0100

    Working on Person Class

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+
+
+class Person
+
+    attr_accessor :name
+
+    def initialize
+        @name = set_name
+      
+    end
+
+    def set_name(input_name)
+    user_name = input_name
+
+    end
+
+
+end
