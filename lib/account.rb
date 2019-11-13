fa28de932eab0d6684feeef248f27d2482292a48
your_name <your@email.com>
2019-11-13 13:38:09 +0100

    account status

--- lib/account.rb
+++ lib/account.rb
@@ -1,12 +1,25 @@
 require 'date'
 
+
 class Account
     STANDARD_VALIDITY_YRS = 5
 
+        attr_accessor :exp_date, :account_status
+        
+
+    def initialize
+        @exp_date = set_expire_date 
+        @account_status = :active
+    end
     def set_expire_date
         Date.today.next_year(Account::STANDARD_VALIDITY_YRS).strftime('%m/%y')
-        
-    end
+     end
+     def deactivate
+        @account_status = :deactivated
+     end
 
+    
 
+    
+    
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -4,15 +4,23 @@ describe Account do
   
 
 
-    it 'check the lenght of the number' do
+    it 'check the length of the number' do
         number = 1234
         number_lengh = Math.log10(number).to_i+1
         expect(number_lengh).to eq 4
     end
 
-    it 'is expected to have ian expiry dato on initialize' do
+    it 'is expected to have an expiry date on initialize' do
         expected_date = Date.today.next_year(5).strftime("%m/%y")
         expect(subject.exp_date).to eq expected_date
     end
+    it 'is expected to have :active status on initialize' do
+        expect(subject.account_status).to eq :active
+    end
+    it 'deactivates account using the instance method' do
+        subject.deactivate
+        expect(subject.account_status).to eq :deactivated
+    end
+
 
 end
