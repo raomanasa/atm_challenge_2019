f0b4ad895b3a1ae13ae78ef8a9b36d3a9354e923
mantanquzzmo <quzzmo@hotmail.com>
2019-11-13 15:06:27 +0100

    Adds account owner and error message

--- lib/account.rb
+++ lib/account.rb
@@ -4,22 +4,32 @@ require 'date'
 class Account
     STANDARD_VALIDITY_YRS = 5
 
-        attr_accessor :exp_date, :account_status
+        attr_accessor :exp_date, :account_status, :owner
         
 
-    def initialize
+    def initialize(attrs = {})
         @exp_date = set_expire_date 
         @account_status = :active
+        set_owner(attrs[:owner])
     end
+
     def set_expire_date
         Date.today.next_year(Account::STANDARD_VALIDITY_YRS).strftime('%m/%y')
-     end
-     def deactivate
+    end
+
+    def deactivate
         @account_status = :deactivated
-     end
+    end 
 
-    
+    private 
+      def set_owner(obj)
+        obj == nil ? missing_owner : @owner = obj
+      end
 
-    
-    
+      def missing_owner
+        raise "An Account owner is required"
+      end
 end
+
+    
+
